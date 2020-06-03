5c70d507810f27b0a71be94f3581fc74fa9385f3
gitHirsi <hirsiboom@163.com>
2020-06-03 22:05:34 +0800

    修改

--- 009集合/01创建集合.py
+++ 009集合/01创建集合.py
@@ -9,16 +9,15 @@ set()集合有去重的特点
 s1 = {10, 20, 30, 40, 50}
 print(s1)
 
-
-s2 = {10, 20, 30, 40, 50,20,30}
+s2 = {10, 20, 30, 40, 50, 20, 30}
 print(s2)
 
-s3=set('abcdefg')
+s3 = set('abcdefg')
 print(s3)
 
-s4=set()
+s4 = set()
 print(s4)
 print(type(s4))
 
-s5={}
+s5 = {}
 print(type(s5))
