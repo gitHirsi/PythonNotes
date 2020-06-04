444192f4e934fe8f445bed03436a696e2c5de76b
gitHirsi <hirsiboom@163.com>
2020-06-04 22:50:05 +0800

    函数...

---
+++ 012函数/03函数的返回值.py
@@ -0,0 +1,26 @@
+"""
+@Author:Hirsi
+@Time:2020/6/4 22:23
+"""
+
+
+# def buy():
+#     return '❤'*5
+#
+#
+# goods = buy()
+# print(goods)
+
+def sumNum(a, b):
+    return a + b
+
+
+numStr = input("""两个数字求和
+请输入(空格隔开): """)
+splitNum = numStr.split(' ')
+a = int(splitNum[0])
+b = int(splitNum[1])
+
+result = sumNum(a, b)
+print(result)
+
---
+++ 012函数/04函数的说明文档.py
@@ -0,0 +1,22 @@
+"""
+@Author:Hirsi
+@Time:2020/6/4 22:43
+"""
+
+# def sun_num(a,b):
+#     """求和函数"""
+#     return a+b
+#
+# help(sun_num)
+
+
+def sum_num1(a,b):
+    """
+    求和函数
+    :param a:
+    :param b:
+    :return:
+    """
+    return a+b
+
+help(sum_num1) #只写函数名没有括号
