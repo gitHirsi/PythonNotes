73ab43233b98de270c6d0090857b7a217d086cdd
gitHirsi <hirsiboom@163.com>
2020-06-10 22:54:21 +0800

    文件操作ing

---
+++ 017文件操作/01体验文件操作步骤.py
@@ -0,0 +1,12 @@
+"""
+@Author:Hirsi
+@Time:2020/6/10 22:23
+"""
+
+# 1.打开 open()
+f = open('test.txt', 'w')
+
+# 2.读写操作 write()  read()
+f.write('aaa')
+# 3.关闭 close()
+f.close()
---
+++ 017文件操作/03读取函数的方法.py
@@ -0,0 +1,37 @@
+"""
+@Author:Hirsi
+@Time:2020/6/10 22:42
+"""
+
+"""
+1.read(num)
+    不写num参数表示读取所有
+"""
+f1=open('test.txt','r')
+print(f1.read(16))
+f1.close()
+
+"""
+2.readlines()
+    按照行的方式把整个文件中的内容进行一次性读取，并且返回一个列表，其中每行数据为一个元素
+"""
+f2=open('test.txt','r')
+readlines = f2.readlines(20)
+print(readlines)
+f2.close()
+
+"""
+3.readline()
+    一次性读取一行内容
+"""
+f3=open('test.txt','r')
+r = f3.readline()
+print(r)
+
+r = f3.readline()
+print(r)
+
+r = f3.readline()
+print(r)
+
+f3.close()
